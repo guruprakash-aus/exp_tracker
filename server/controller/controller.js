512b5fe731f7d6b86c2c12b84c7f5351db43b9ca
guruprakash-aus <rajendranguruprakash@gmail.com>
2022-03-17 23:45:30 +1100

    Backend API Routes created

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,8 +1,120 @@
-// get categories
-const createCategories = (req, res) => {
-  res.json("Get Request from Categories");
+const model = require("../models/model");
+
+// post: http://localhost:8080/api/categories
+const createCategories = async (req, res) => {
+  const Create = new model.Categories({
+    type: "Investment",
+    color: "#FCBE44", // dark
+  });
+
+  await Create.save(function (err) {
+    if (!err) return res.json(Create);
+    return res
+      .status(400)
+      .json({ message: `Error while creating categories ${err}` });
+  });
+};
+
+// get:  http://localhost:8080/api/categories
+const getCategories = async (req, res) => {
+  let data = await model.Categories.find({});
+
+  let filter = data.map((v) =>
+    Object.assign({}, { type: v.type, color: v.color })
+  );
+  return res.json(filter);
+};
+
+// post:  http://localhost:8080/api/transaction
+const createTransaction = async (req, res) => {
+  if (!req.body) return res.status(400).send("Request body is missing in Post");
+  let { name, type, amount } = req.body;
+
+  const Create = await new model.Transaction({
+    name,
+    type,
+    amount,
+    date: new Date(),
+  });
+
+  Create.save(function (err) {
+    if (!err) return res.json(Create);
+    return res
+      .status(400)
+      .json({ message: `Error while creating Transaction ${err}` });
+  });
+};
+
+// get:  http://localhost:8080/api/transaction
+const getTransaction = async (req, res) => {
+  let data = await model.Transaction.find({});
+
+  return res.json(data);
+};
+
+// delete: http://localhost:8080/api/transaction
+const deleteTransaction = async (req, res) => {
+  if (!req.body)
+    return res.status(400).send("Request body is missing in Delete");
+  //   let { id } = req.body;
+
+  // await model.Transaction.findByIdAndDelete(id, function (err) {
+  //     if (!err) return res.json({ message: "Transaction Deleted" });
+  //     return res
+  //         .status(400)
+  //         .json({ message: `Error while deleting Transaction ${err}` });
+  // });
+
+  await model.Transaction.deleteOne(req.body, function (err) {
+    if (!err) res.json("Transaction Record Deleted...!");
+  })
+    .clone()
+    .catch(function (err) {
+      res.json(`Error while deleting Transaction Record ${err}`);
+    });
+};
+
+// get : http://localhost:8080/api/labels
+const getLabels = async (req, res) => {
+  let data = await model.Transaction.aggregate([
+    {
+      //key
+      $lookup: {
+        from: "categories",
+        localField: "type",
+        foreignField: "type",
+        as: "categoriesInfo",
+      },
+    },
+    {
+      $unwind: "$categoriesInfo",
+    },
+  ])
+    .then((result) => {
+      let data = result.map((v) =>
+        Object.assign(
+          {},
+          {
+            _id: v._id,
+            name: v.name,
+            type: v.type,
+            amount: v.amount,
+            color: v.categoriesInfo.color,
+          }
+        )
+      );
+      res.json(data);
+    })
+    .catch((error) => {
+      res.status(400).json(`Lookup Collection Error ${error}`);
+    });
 };
 
 module.exports = {
   createCategories,
+  getCategories,
+  createTransaction,
+  getTransaction,
+  deleteTransaction,
+  getLabels,
 };
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,17 @@
 const routes = require("express").Router();
 const controller = require("../controller/controller");
 
-routes.route("/api/categories").get(controller.createCategories);
+routes
+  .route("/api/categories")
+  .get(controller.getCategories)
+  .post(controller.createCategories);
+
+routes
+  .route("/api/transaction")
+  .post(controller.createTransaction)
+  .get(controller.getTransaction)
+  .delete(controller.deleteTransaction);
+
+routes.route("/api/labels").get(controller.getLabels);
 
 module.exports = routes;
